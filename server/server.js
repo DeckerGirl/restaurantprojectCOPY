75fae6a896138de25dc4c15cae2273229a17e07e
Amber Spath <122047785+DeckerGirl@users.noreply.github.com>
2026-03-11 11:35:17 -0400

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient } = require("mongodb");
+const { MongoClient, ObjectId } = require("mongodb");
 const app = express();
 const PORT = 5000;
 
@@ -12,7 +12,9 @@ app.get("/", (req, res) => {
   res.send("Server is running");
 });
 
-// Menu routes
+// ========== MENU ROUTES ==========
+
+// GET all menu items
 app.get("/api/menu", async (req, res) => {
   try {
     let collection = db.collection("Menu");
@@ -23,10 +25,47 @@ app.get("/api/menu", async (req, res) => {
   }
 });
 
+// GET a single menu item by ID
+app.get("/api/menu/:id", async (req, res) => {
+  try {
+    let collection = db.collection("Menu");
+    let result = await collection.findOne({ _id: new ObjectId(req.params.id) });
+    res.json(result);
+  } catch(e) {
+    console.log(e);
+  }
+});
+
+// POST - add a new menu item
 app.post("/api/menu", async (req, res) => {
   try {
     let collection = db.collection("Menu");
     let result = await collection.insertOne(req.body);
+    res.json(result);
+  } catch(e) {
+    console.log(e);
+  }
+});
+
+// PUT - update a menu item by ID
+app.put("/api/menu/:id", async (req, res) => {
+  try {
+    let collection = db.collection("Menu");
+    let result = await collection.updateOne(
+      { _id: new ObjectId(req.params.id) },
+      { $set: req.body }
+    );
+    res.json(result);
+  } catch(e) {
+    console.log(e);
+  }
+});
+
+// DELETE - remove a menu item by ID
+app.delete("/api/menu/:id", async (req, res) => {
+  try {
+    let collection = db.collection("Menu");
+    let result = await collection.deleteOne({ _id: new ObjectId(req.params.id) });
     res.json(result);
   } catch(e) {
     console.log(e);
