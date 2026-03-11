85be28c6f10ce9f7faa8f1f32e3b052f3d0081e0
Amber Spath <122047785+DeckerGirl@users.noreply.github.com>
2026-03-11 11:36:51 -0400

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import AdminDashboard from "./pages/AdminDashboard";
 import UserDashboard from "./pages/UserDashboard";
+import MenuPage from "./pages/MenuPage";
 import "./App.css";
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<LoginPage />} />
         <Route path="/admin" element={<AdminDashboard />} />
         <Route path="/user" element={<UserDashboard />} />
+        <Route path="/menu" element={<MenuPage />} />
       </Routes>
     </Router>
   );
